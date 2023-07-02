996215d36e2f7028b4c97e9d38e755ffb40c418f
Raunit Singh <raunitsingh@Raunits-MacBook-Air.local>
2023-07-02 15:39:20 +0530

    minor change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar({ bgColor, color }) {
   
   return (
     <nav className='navbar w-full flex px-10 py-10 justify-between items-center' style={{ backgroundColor: bgColor, color: color }}>
-      <h1 className='w-[95px] h-[32px] ' alt="logo"><Link to='/'>ROBOTICS 101</Link></h1>
+      <h1 className=' h-[32px] ' alt="logo"><Link to='/'>ROBOTICS<span className='ml-1'>101</span></Link></h1>
       <ul className="list-none flex flex-1 px-[15%] justify-around  items-center">
         <li>
-          <Link to='/events'>EVENTS</Link>
+          <Link  to='/events'>EVENTS</Link>
         </li>
         <li>
           <Link to='/leaderboard'>LEADERBOARD</Link>
