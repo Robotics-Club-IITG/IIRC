71ae73090e06ed853e48ea399e9d6f0698304b28
Raunit Singh <s.raunit@iitg.ac.in>
2023-08-15 18:21:30 +0530

    mc

--- client/src/screens/LeaderBoard.jsx
+++ client/src/screens/LeaderBoard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./LeaderBoard.css";
 import Navbar from "../components/Navbar";
+import leaderData from '../data/leaderboard'
 import LeaderBoardNav from "../components/LeaderBoardNav";
 import { leaderBoardData } from '../data/leaderBoardData';
 import { popupDetails } from "../components/themeDetails";
@@ -11,15 +12,17 @@ function LeaderBoard() {
   const [active,setActive] = useState(0);
   const { bgLeft,bgRight } = popupDetails[active];
   const {color} = cardDetails[active];
-	const LeaderBoardItem = ({ rank, name, points }) => {
+	const LeaderBoardItem = ({ rank, name, points,course }) => {
 		return (
-			<div className="leaderBoard__stats_content_item p-2">
+			<div className="leaderBoard__stats_content_item p-2 my-1">
 				<h1>{rank}</h1>
 				<h1>{name}</h1>
+				<h1>{course}</h1>
 				<h1 className="text-right">{points}</h1>
 			</div>
 		);
 	};
+	console.log(leaderData);
 	return (
 		<div className="leaderBoard" style={{backgroundColor : bgLeft}}>
 			<div className="w-full">
@@ -30,18 +33,16 @@ function LeaderBoard() {
 					<div className="leaderBoard__stats_title p-2">
 						<h1>RANK</h1>
 						<h1>NAME</h1>
+						<h1>COURSE</h1>
 						<h1 className="text-right">POINTS</h1>
 					</div>
 					<div className="leaderBoard__stats_contentWrap">
             <div className="leaderBoard__stats_content my-6 spy1 flex-1">
-              {leaderBoardData[active].map((item, index) => {
-                return <LeaderBoardItem key={index} rank={item.rank} name={item.name} points={item.points} />
+              {leaderData.map((item, index) => {
+                return <LeaderBoardItem key={index} rank={index+1} name={item.name} points={item.total} course={item.course} />
               })}
     
             </div>
-          </div>
-          <div className="w-[60%]" >
-            <LeaderBoardNav style={{backgroundColor : bgRight}} active={active} setActive={setActive} />
           </div>
 				</div>
 			</div>
